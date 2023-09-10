0cc31fb3fda8044d323e5d6811a81dcbd5cf781b
Gitkontrol <cloud.ayo1@gmail.com>
2023-09-09 21:28:04 -0400

    gh action

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ command }) => {
+
+  const config = {
+
+      plugins: [react(), tailwindcss("./tailwind.config.js")],
+
+      base: "/",
+
+  };
+
+
+
+
+  if (command !== "serve") {
+
+      config.base = "/countries_api_app/";
+
+  }
+
+
+
+
+  return config;
+
+});
